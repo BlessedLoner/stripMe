673f6bdbd518bd04e7b5a4885f011edfc1458be1
BlessedLoner <biggestblessed20@gmail.com>
2026-06-10 11:17:37 +0100

    Fix Slider.css import for Vercel deployment

--- src/components/SlideingCard.jsx
+++ src/components/SlideingCard.jsx
@@ -1,19 +1,8 @@
 import React, { useEffect, useState } from "react";
-import "./slider.css"; // we’ll add this next
+import "./Slider.css"; // we’ll add this next
 import { members } from "../data/members";
 
 export default function SlideingCard() {
-  //   const images = [
-  //     "https://images.unsplash.com/photo-1517841905240-472988babdf9",
-  //     "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d",
-  //     "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
-  //     "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
-  //     "https://images.unsplash.com/photo-1494790108377-be9c29b29330",
-  //     "https://images.unsplash.com/photo-1500648767791-00dcc994a43e",
-  //     "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e",
-  //     "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d",
-  //   ];
-
   return (
     <div className="slider-container overflow-hidden w-full bg-gradient-to-r from-pink-50 to-rose-50 py-4">
       <div className="slider-track flex gap-4 animate-slide">
