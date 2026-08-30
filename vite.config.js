4f4af98932d95a8baeec95be7f0179d530ab6414
BlessedLoner <biggestblessed20@gmail.com>
2026-08-30 10:45:10 +0100

    Did same old chnages

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,5 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-
-  console.log("========== VITE BUILD DEBUG ==========");
-console.log(
-  "STRIPE KEY EXISTS:",
-  Boolean(process.env.VITE_STRIPE_PUBLISHABLE_KEY),
-);
-console.log(
-  "STRIPE KEY PREFIX:",
-  process.env.VITE_STRIPE_PUBLISHABLE_KEY?.slice(0, 7),
-);
-console.log("======================================");
-
   plugins: [react(), tailwindcss()],
 });
