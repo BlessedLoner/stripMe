36422d94165d2f4e8e8d27ded5add0d4063019a3
BlessedLoner <biggestblessed20@gmail.com>
2026-08-30 11:16:05 +0100

    debug production stripe environment

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,32 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(() => {
+  console.log("========== VITE BUILD DEBUG ==========");
+
+  console.log(
+    "STRIPE KEY EXISTS:",
+    Boolean(process.env.VITE_STRIPE_PUBLISHABLE_KEY),
+  );
+
+  console.log(
+    "STRIPE KEY PREFIX:",
+    process.env.VITE_STRIPE_PUBLISHABLE_KEY?.slice(0, 7),
+  );
+
+  console.log("======================================");
+
+  return {
+    plugins: [react(), tailwindcss()],
+  };
 });
+
+
+// import { defineConfig } from "vite";
+// import react from "@vitejs/plugin-react";
+// import tailwindcss from "@tailwindcss/vite";
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react(), tailwindcss()],
+// });
